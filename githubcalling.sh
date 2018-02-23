d5e7198d0e0ff9c21edb0cc18202cedcfbd5f890
jlmartinezmarin <jmarti73@hawk.iit.edu>
2018-02-23 17:26:57 -0600

    antoher day

---
+++ githubcalling.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#cd "$Home/jmarti73/project57"
+
+alias proj="cd /home/jmarti73/project571"
+
+git init
+
+git remote add upstream https://github.com/rbuezotordesillas/project571.git
+
+git checkout virtualmachine
+
+sudo git add .
+
+echo Hello world
+
+git commit -m "antoher day"
+
+sudo git push origin virtualmachine
+
+
+
+
